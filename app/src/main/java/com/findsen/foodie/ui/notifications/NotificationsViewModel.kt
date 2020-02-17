fac028f6e664dc82110e14a4a3305297fd0888af
Findsen <chr.findsen@gmail.com>
2020-02-18 00:49:17 +0100

    First commit

---
+++ app/src/main/java/com/findsen/foodie/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.findsen.foodie.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
